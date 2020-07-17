07bdc31125335c2a43bf2ea46c4f13ce4ccdd3b7
Tsutsujun <kattou.surumono@gmail.com>
2020-07-17 14:22:44 +0900

    edits a migration-file of groups-table

--- db/migrate/20200717042535_create_groups.rb
+++ db/migrate/20200717042535_create_groups.rb
@@ -1,7 +1,8 @@
 class CreateGroups < ActiveRecord::Migration[5.0]
   def change
     create_table :groups do |t|
-
+      t.string :title, null: false
+      t.index  :title, unique: true
       t.timestamps
     end
   end

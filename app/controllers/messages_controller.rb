13ba0b4ec61ac39bbb270034c40b1ca192822ddd
Tsutsujun <kattou.surumono@gmail.com>
2020-08-14 20:41:50 +0900

    makes the contents of create-action
    ストロングパラメータも設定する。

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,6 +4,13 @@ class MessagesController < ApplicationController
   end
 
   def create
+    Message.create(message_params)
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:body, :image).merge(group_id: params[:group_id], user_id: current_user.id)
   end
 
 end

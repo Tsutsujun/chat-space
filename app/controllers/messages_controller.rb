da5b45c8e61b610661adb4b6a2859be595b56f8b
Tsutsujun <kattou.surumono@gmail.com>
2020-08-14 21:00:47 +0900

    fixes message_params
    また、createアクションの中でインスタンス変数@groupを生成する。

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,13 +4,14 @@ class MessagesController < ApplicationController
   end
 
   def create
+    @group = Group.find(params[:group_id])
     Message.create(message_params)
   end
 
   private
 
   def message_params
-    params.require(:message).permit(:body, :image).merge(group_id: params[:group_id], user_id: current_user.id)
+    params.require(:message).permit(:body, :image).merge(user_id: current_user.id)
   end
 
 end

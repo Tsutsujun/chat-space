29c1b4d0a61bd63e38569275e9c822f257f08e91
Tsutsujun <kattou.surumono@gmail.com>
2020-08-17 16:37:06 +0900

    edits the contents of index and create action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,6 +4,7 @@ class MessagesController < ApplicationController
 
   def index
     @message = Message.new
+    @messages = @group.messages.includes(:user)
   end
 
   def create
@@ -11,6 +12,7 @@ class MessagesController < ApplicationController
     if @message.save
       redirect_to group_messages_path(@group), notice: "メッセージが送信されました。"
     else
+      @messages = @group.messages.includes(:user)
       flash.now[:alert] = "メッセージを入力してください。"
       render :index
     end

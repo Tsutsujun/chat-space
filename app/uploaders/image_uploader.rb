2a48c351404338de0954783a40d3060be2bb89ae
Tsutsujun <kattou.surumono@gmail.com>
2020-08-24 15:32:32 +0900

    shows message-index of the current_user-group

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -28,12 +28,12 @@ class ImageUploader < CarrierWave::Uploader::Base
   #   # do something
   # end
 
+  process resize_to_fit: [800, 800]
+
   # Create different versions of your uploaded files:
   # version :thumb do
   #   process resize_to_fit: [50, 50]
   # end
-
-  process resize_to_fit: [800, 800]
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:

57617f9f4a89a7a8c3b1a9ae1ed0351f227f21e0
Tsutsujun <kattou.surumono@gmail.com>
2020-09-07 16:25:36 +0900

    makes set_messages for before_action
    更に可読性を向上させる。

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,10 @@
 class MessagesController < ApplicationController
 
   before_action :set_group
+  before_action :set_messages
 
   def index
     @message = Message.new
-    @messages = @group.messages.includes(:user)
   end
 
   def create
@@ -13,7 +13,6 @@ class MessagesController < ApplicationController
       flash[:notice] = "メッセージが送信されました。"
       redirect_to group_messages_path(@group)
     else
-      @messages = @group.messages.includes(:user)
       flash.now[:alert] = "メッセージを入力してください。"
       render :index
     end
@@ -28,6 +27,10 @@ class MessagesController < ApplicationController
 
   def set_group
     @group = Group.find(params[:group_id])
+  end
+
+  def set_messages
+    @messages = @group.messages.includes(:user)
   end
 
 end

f1eea72fd33aa6ce6466cd6c05f247a65f5c9ce7
Tsutsujun <kattou.surumono@gmail.com>
2020-09-09 15:16:20 +0900

    defines show_last_message method

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,4 +4,11 @@ class Group < ApplicationRecord
   has_many :messages
 
   validates :title, presence: true, uniqueness: true
+
+  def show_last_message
+    - unless group.messages.empty?
+      = group.messages.last.body.presence || "画像が投稿されています。"
+    - else
+      まだメッセージはありません。
+  end
 end

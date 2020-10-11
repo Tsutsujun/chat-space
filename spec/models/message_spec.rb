3e3c1339743914e75e638fe8d99297e0803edcf3
Tsutsujun <kattou.surumono@gmail.com>
2020-10-11 21:22:05 +0900

    makes a example "画像があれば保存できる。"

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -6,6 +6,11 @@ describe Message do
         message = build(:message, image: "")
         expect(message).to be_valid
       end
+
+      it "画像があれば保存できる。" do
+        message = build(:message, body: "")
+        expect(message).to be_valid
+      end
     end
 
     describe "メッセージを保存できない場合" do

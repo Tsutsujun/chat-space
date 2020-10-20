057b0885a1b3b3de2bd13dfacb8a6ec6c8d47ce9
Tsutsujun <kattou.surumono@gmail.com>
2020-10-20 15:14:57 +0900

    makes a example "テキスト(body)と画像(image)が両方とも無かったら保存できないこと"
    また、各exampleの説明文をより詳細にした。
    更に、「image」カラムの値を空文字“”に上書きしても結局nilになるので変更した。

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,25 +1,29 @@
 describe Message do
   describe '#save' do
 
-    describe "メッセージを保存できる場合" do
-      it "テキストがあれば保存できる。" do
-        message = build(:message, image: "")
+    describe "メッセージ(message)を保存できる場合" do
+      it "テキスト(body)があれば保存できること" do
+        message = build(:message, image: nil)
         expect(message).to be_valid
       end
 
-      it "画像があれば保存できる。" do
+      it "画像(image)があれば保存できること" do
         message = build(:message, body: "")
         expect(message).to be_valid
       end
 
-      it "テキストと画像があれば保存できる。" do
+      it "テキスト(body)と画像(image)が両方あれば保存できること" do
         message = build(:message)
         expect(message).to be_valid
       end
     end
 
-    describe "メッセージを保存できない場合" do
-
+    describe "メッセージ(message)を保存できない場合" do
+      it "テキスト(body)と画像(image)が両方とも無かったら保存できないこと" do
+        message = build(:message, body: "", image: nil)
+        message.valid?
+        expect(message.errors[:body]).to include("を入力してください")
+      end
     end
 
   end

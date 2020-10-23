cff23c76dbad07aa406b8af527a44a47cf784a81
Tsutsujun <kattou.surumono@gmail.com>
2020-10-23 16:06:01 +0900

    makes a ExampleGroup "アクション内で定義しているインスタンス変数に正しい値が入っているか" with describe
    また、２つ目のdescribeの説明にHTTPメソッドの'GET'を付けていなかったので補足した。

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,7 +1,10 @@
 RSpec.describe MessagesController, type: :controller do
-  describe '#index' do
+  describe 'GET #index' do
 
     context "ログインしている場合" do
+      describe "アクション内で定義しているインスタンス変数に正しい値が入っているか" do
+
+      end
 
     end
 

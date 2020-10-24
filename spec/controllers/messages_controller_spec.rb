05dad1ced7857f6afe60181cb5567d768c9570b6
Tsutsujun <kattou.surumono@gmail.com>
2020-10-24 18:02:48 +0900

    makes a Example "@groupに期待した値が入っていること"

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -3,6 +3,13 @@ RSpec.describe MessagesController, type: :controller do
 
     context "ログインしている場合" do
       describe "アクション内で定義しているインスタンス変数に正しい値が入っているか" do
+        it "@groupに期待した値が入っていること" do
+          user  = create(:user)
+          group = create(:group)
+          login user
+          get :index, params: { group_id: group.id }
+          expect(assigns(:group)).to eq group
+        end
 
       end
 

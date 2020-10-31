22a12243568fb3981d1a36a8d5193c9088b00981
Tsutsujun <kattou.surumono@gmail.com>
2020-11-01 04:03:14 +0900

    modifies name and email column of user-factory
    次のExample"@messagesに期待した値が入っていること"の下準備。
    「name」と「email」カラムの値を連番にする。

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
-    name                  {Faker::Name.last_name}
-    email                 {Faker::Internet.free_email}
+    sequence(:name,  Faker::Name.last_name)
+    sequence(:email, Faker::Internet.free_email)
     password = Faker::Internet.password(min_length: 8)
     password              {password}
     password_confirmation {password}

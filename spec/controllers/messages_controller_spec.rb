6d66c6dfc8b31e105759a354320024f84b330218
Tsutsujun <kattou.surumono@gmail.com>
2020-11-01 08:58:07 +0900

    rewrites the variables in messages_controller_spec.rb using let-method

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,9 +1,11 @@
 RSpec.describe MessagesController, type: :controller do
   describe 'GET #index' do
 
+    let(:group) { create(:group) }
+    let(:user)  { create(:user) }
+
     context "ログインしていない場合" do
       it "ログイン画面にリダイレクトすること" do
-        group = create(:group)
         get :index, params: { group_id: group.id }
         expect(response).to redirect_to(new_user_session_path)
       end
@@ -12,24 +14,18 @@ RSpec.describe MessagesController, type: :controller do
     context "ログインしている場合" do
       describe "アクション内で定義しているインスタンス変数に正しい値が入っているか" do
         it "@groupに期待した値が入っていること" do
-          user  = create(:user)
-          group = create(:group)
           login user
           get :index, params: { group_id: group.id }
           expect(assigns(:group)).to eq group
         end
 
         it "@messageに期待した値が入っていること" do
-          user  = create(:user)
-          group = create(:group)
           login user
           get :index, params: { group_id: group.id }
           expect(assigns(:message)).to be_a_new(Message)
         end
 
         it "@messagesに期待した値が入っていること" do
-          user  = create(:user)
-          group = create(:group)
           messages = create_list(:message, 3, group: group)
           login user
           get :index, params: { group_id: group.id }
@@ -38,8 +34,6 @@ RSpec.describe MessagesController, type: :controller do
       end
 
       it "index.html.hamlに遷移すること" do
-        user  = create(:user)
-        group = create(:group)
         login user
         get :index, params: { group_id: group.id }
         expect(response).to render_template :index

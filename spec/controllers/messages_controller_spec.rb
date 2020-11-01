de8e3b87317902cca3a6453db960253721ca62d8
Tsutsujun <kattou.surumono@gmail.com>
2020-11-01 09:28:38 +0900

    rewrites the common process in messages_controller_spec.rb using before-method

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -12,30 +12,27 @@ RSpec.describe MessagesController, type: :controller do
     end
 
     context "ログインしている場合" do
+      before do
+        login user
+        get :index, params: { group_id: group.id }
+      end
+
       describe "アクション内で定義しているインスタンス変数に正しい値が入っているか" do
         it "@groupに期待した値が入っていること" do
-          login user
-          get :index, params: { group_id: group.id }
           expect(assigns(:group)).to eq group
         end
 
         it "@messageに期待した値が入っていること" do
-          login user
-          get :index, params: { group_id: group.id }
           expect(assigns(:message)).to be_a_new(Message)
         end
 
         it "@messagesに期待した値が入っていること" do
           messages = create_list(:message, 3, group: group)
-          login user
-          get :index, params: { group_id: group.id }
           expect(assigns(:messages)).to match(group.messages)
         end
       end
 
       it "index.html.hamlに遷移すること" do
-        login user
-        get :index, params: { group_id: group.id }
         expect(response).to render_template :index
       end
     end

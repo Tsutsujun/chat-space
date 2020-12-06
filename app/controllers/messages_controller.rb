8b04b916ff2afd55d771d54ec5a5995551b0c23b
Tsutsujun <kattou.surumono@gmail.com>
2020-12-06 22:47:09 +0900

    improves messages_controller.rb
    jbuilderを使う、且つ常にJSON形式でデータを返すようにリクエストする場合は、「respond_to」メソッドは不要なので省略した。

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,11 +9,7 @@ class MessagesController < ApplicationController
 
   def create
     @message = @group.messages.new(message_params)
-    if @message.save
-      respond_to do |format|
-        format.json
-      end
-    else
+    unless @message.save
       flash.now[:alert] = "メッセージを入力してください。"
       render :index
     end

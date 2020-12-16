ac2920dc5a55c9b06e150bb04a76d25e1053e576
Tsutsujun <kattou.surumono@gmail.com>
2020-12-16 15:59:15 +0900

    improves buildHTML-function

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,47 +1,37 @@
 $(function () {
 
   function buildHTML(message) {
-    if (message.text && message.image) {
-      return `<div class="MessageInfo">` +
-        `<div class="MessageInfo__TopBox">` +
-        `<p class="MessageInfo__TopBox--Name">` +
-        message.name +
-        `</p>` +
-        `<p class="MessageInfo__TopBox--Date">` +
-        message.time +
-        `</p>` +
-        `</div>` +
-        `<p class="MessageInfo--Text">` +
-        message.text +
-        `</p>` +
-        `<img src="` + message.image + `"class="MessageInfo--Img">` +
-        `</div>`;
-    } else if (message.text) {
-      return `<div class="MessageInfo">` +
-        `<div class="MessageInfo__TopBox">` +
-        `<p class="MessageInfo__TopBox--Name">` +
-        message.name +
-        `</p>` +
-        `<p class="MessageInfo__TopBox--Date">` +
-        message.time +
-        `</p>` +
-        `</div>` +
-        `<p class="MessageInfo--Text">` +
-        message.text +
-        `</p>` +
-        `</div>`;
-    } else if (message.image) {
-      return `<div class="MessageInfo">` +
-        `<div class="MessageInfo__TopBox">` +
-        `<p class="MessageInfo__TopBox--Name">` +
-        message.name +
-        `</p>` +
-        `<p class="MessageInfo__TopBox--Date">` +
-        message.time +
-        `</p>` +
-        `</div>` +
-        `<img src="` + message.image + `"class="MessageInfo--Img">` +
-        `</div>`;
+    if (message.image) {
+      return
+        `<div class="MessageInfo">
+          <div class="MessageInfo__TopBox">
+            <p class="MessageInfo__TopBox--Name">
+              ${message.name}
+            </p>
+            <p class="MessageInfo__TopBox--Date">
+              ${message.time}
+            </p>
+          </div>
+          <p class="MessageInfo--Text">
+            ${message.text}
+          </p>
+          <img src=${message.image} class="MessageInfo--Img">
+        </div>`;
+    } else {
+      return
+        `<div class="MessageInfo">
+          <div class="MessageInfo__TopBox">
+            <p class="MessageInfo__TopBox--Name">
+              ${message.name}
+            </p>
+            <p class="MessageInfo__TopBox--Date">
+              ${message.time}
+            </p>
+          </div>
+          <p class="MessageInfo--Text">
+            ${message.text}
+          </p>
+        </div>`;
     };
   };
 

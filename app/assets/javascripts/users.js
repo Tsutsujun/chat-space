28b9df4686db58c9782a9cc7bcd9148b4e6f6e23
Tsutsujun <kattou.surumono@gmail.com>
2020-12-31 17:58:01 +0900

    uses the ajax-method

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,5 +2,11 @@ $(function () {
 
   $('#user_search').on('keyup', function () {
     var input = $(this).val();
+    $.ajax({
+      url: '/users',
+      type: 'GET',
+      data: { keyword: input },
+      dataType: 'json'
+    })
   });
 });

90aa12c74a20a1dd9d78809f71546c7766c133ad
Tsutsujun <kattou.surumono@gmail.com>
2021-01-02 21:13:01 +0900

    the value of @users from database scraping-test by the index-action
    また、ajax通信に成功した場合、失敗した場合の処理をそれぞれconsole.logで仮置きしている。

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -8,5 +8,11 @@ $(function () {
       data: { keyword: input },
       dataType: 'json'
     })
+    .done(function (users) {
+      console.log("成功です。");
+    })
+    .fail(function () {
+      console.log("失敗です。");
+    });
   });
 });

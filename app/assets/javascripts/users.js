c54d25786072a0bf1754f2e09b6a2aaff883df06
Tsutsujun <kattou.surumono@gmail.com>
2021-01-17 16:00:09 +0900

    returns html-value at the end of each defined function

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -5,12 +5,14 @@ $(function () {
                   <p class="chat-group-user__name">${user.name}</p>
                   <a class="chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
                 </div>`;
+    return html;
   };
 
   function appendErrMsg(msg) {
     var html = `<div class="chat-group-user">
                   <p class="chat-group-user__name">${msg}</p>
                 </div>`;
+    return html;
   };
 
   $('#user_search').on('keyup', function () {
